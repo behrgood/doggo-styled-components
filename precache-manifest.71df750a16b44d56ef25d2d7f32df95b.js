0f90952505d8a9d9d2233b43a854114b7e972fe0
behrgood <behrgood@gmail.com>
2019-03-19 22:49:33 -0400

    Updates

---
+++ precache-manifest.71df750a16b44d56ef25d2d7f32df95b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/doggo-styled-components/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "7125ced549a823d784e5",
+    "url": "/doggo-styled-components/static/js/runtime~main.d9dde9c0.js"
+  },
+  {
+    "revision": "c34cbacf1ec2a8aa742f",
+    "url": "/doggo-styled-components/static/js/main.5462d211.chunk.js"
+  },
+  {
+    "revision": "77338693fff191308231",
+    "url": "/doggo-styled-components/static/js/2.149fe287.chunk.js"
+  },
+  {
+    "revision": "c34cbacf1ec2a8aa742f",
+    "url": "/doggo-styled-components/static/css/main.78d8eca6.chunk.css"
+  },
+  {
+    "revision": "33ea85742cb0ecc36c408580a2c14371",
+    "url": "/doggo-styled-components/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/doggo-styled-components/precache-manifest.71df750a16b44d56ef25d2d7f32df95b.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/doggo-styled-components/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});

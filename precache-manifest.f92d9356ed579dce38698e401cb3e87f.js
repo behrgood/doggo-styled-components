6595e1f12604524d84bd4248da617e727eb4a492
behrgood <behrgood@gmail.com>
2019-03-19 23:00:32 -0400

    Updates

--- precache-manifest.f92d9356ed579dce38698e401cb3e87f.js
+++ precache-manifest.f92d9356ed579dce38698e401cb3e87f.js
@@ -8,19 +8,19 @@ self.__precacheManifest = [
     "url": "/doggo-styled-components/static/js/runtime~main.d9dde9c0.js"
   },
   {
-    "revision": "8a402cb670c3d78237ae",
-    "url": "/doggo-styled-components/static/js/main.52e1a9d0.chunk.js"
+    "revision": "f5fd0baf687e8da0908a",
+    "url": "/doggo-styled-components/static/js/main.8a192f1a.chunk.js"
   },
   {
     "revision": "77338693fff191308231",
     "url": "/doggo-styled-components/static/js/2.149fe287.chunk.js"
   },
   {
-    "revision": "8a402cb670c3d78237ae",
+    "revision": "f5fd0baf687e8da0908a",
     "url": "/doggo-styled-components/static/css/main.d425387e.chunk.css"
   },
   {
-    "revision": "8611ac086be15ed9734d5bdeb57ca061",
+    "revision": "3c406375159d9fa8d4efdb2049ab6efb",
     "url": "/doggo-styled-components/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/doggo-styled-components/precache-manifest.7d5257082d3ea30609a9e2e4fc7392a0.js"
+  "/doggo-styled-components/precache-manifest.f92d9356ed579dce38698e401cb3e87f.js"
 );
 
 workbox.clientsClaim();
